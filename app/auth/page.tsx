c9ef277e0c041bdddf8c3db5eb82cc7cdf850534
plusultratablet1-tech <plusultratablet1@gmail.com>
2026-09-02 16:31:26 +0800

    Delete app directory

--- app/auth/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Auth Page ✅</h1>
-    </main>
-  );
-}
